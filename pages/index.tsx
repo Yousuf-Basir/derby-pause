c5fd473a106ed1033f5443b4c52a886f31d44cd6
yousuf basir <yousufbasir@outlook.com>
2022-10-27 01:28:53 +0600

    feat: update play behavior

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,66 +11,82 @@ export default function Home(props: Props) {
   const { videoList } = props;
   const [isPauseDown, setIsPauseDown] = useState(false);
 
-  const primaryVideoElement = useRef<HTMLVideoElement>(null);
-  const primaryVideoSourceElement = useRef<HTMLSourceElement>(null);
-  const [primaryVideoIndex, setPrimaryVideoIndex] = useState(0);
+  const [slotIndex, setSlotIndex] = useState(0);
 
-  const secondaryVideoElement = useRef<HTMLVideoElement>(null);
-  const secondaryVideoSourceElement = useRef<HTMLSourceElement>(null);
-  const [secondaryVideoIndex, setSecondaryVideoIndex] = useState(0);
-
-  const updatePrimaryVideoIndex = () => {
-    if (primaryVideoIndex + 1 < videoList.length) {
-      setPrimaryVideoIndex((prevValue) => (prevValue + 1));
+  const updateSlotIndex = () => {
+    if (slotIndex < videoList.length) {
+      setSlotIndex((prevValue) => (prevValue + 1));
     } else {
-      setPrimaryVideoIndex(0);
+      setSlotIndex(0);
     }
   }
 
   useEffect(() => {
-    primaryVideoSourceElement.current
-      ?.setAttribute('src', videoList[primaryVideoIndex]?.primaryVideo || '');
-
-    secondaryVideoElement.current
-      ?.setAttribute('src', videoList[primaryVideoIndex]?.secondaryVideo || '');
-
-    primaryVideoElement.current?.load();
-    primaryVideoElement.current?.play();
-  }, [primaryVideoIndex])
+    const primaryVideoPlayer: HTMLVideoElement = document.getElementById(`slot${slotIndex}-primary`) as HTMLVideoElement;
+    const secondaryVideoPlayer: HTMLVideoElement = document.getElementById(`slot${slotIndex}-secondary`) as HTMLVideoElement;
+    if (isPauseDown) {
+      secondaryVideoPlayer.play();
+    } else {
+      primaryVideoPlayer.play();
+    }
+  }, [slotIndex])
 
   const handlePauseDown = () => {
+    if (!videoList[slotIndex].secondaryVideo) {
+      return;
+    }
+
     setIsPauseDown(true);
-    primaryVideoElement.current?.play();
-    secondaryVideoElement.current?.play();
+    const primaryVideoPlayer: HTMLVideoElement = document.getElementById(`slot${slotIndex}-primary`) as HTMLVideoElement;
+    const secondaryVideoPlayer: HTMLVideoElement = document.getElementById(`slot${slotIndex}-secondary`) as HTMLVideoElement;
+    primaryVideoPlayer.pause();
+    secondaryVideoPlayer.play();
   }
 
   const handlePauseUp = () => {
+    if (!videoList[slotIndex].secondaryVideo) {
+      return;
+    }
+    
     setIsPauseDown(false);
+    const primaryVideoPlayer: HTMLVideoElement = document.getElementById(`slot${slotIndex}-primary`) as HTMLVideoElement;
+    const secondaryVideoPlayer: HTMLVideoElement = document.getElementById(`slot${slotIndex}-secondary`) as HTMLVideoElement;
+    primaryVideoPlayer.play();
+    secondaryVideoPlayer.pause();
   }
 
   return (
     <div className='homePageRoot'>
       <div className='videoContainer'>
-        <video
-          width='100%'
-          controls
-          ref={primaryVideoElement}
-          onEnded={() => updatePrimaryVideoIndex()}
-          className={isPauseDown && videoList[primaryVideoIndex].secondaryVideo?'hide':'show'}
-        >
-          <source ref={primaryVideoSourceElement} src={videoList[0].primaryVideo}></source>
-        </video>
+        {
+          videoList.map((slot, key) => (
+            <video
+              id={`slot${key}-primary`}
+              className={!isPauseDown && slotIndex == key ? 'show' : 'hide'}
+              onEnded={() => updateSlotIndex()}
+              key={key}
+              muted
+              autoPlay
+            >
+              <source src={slot.primaryVideo} />
+            </video>
+          ))
+        }
 
-        <video
-          width='100%'
-          controls
-          ref={secondaryVideoElement}
-          onEnded={() => updatePrimaryVideoIndex()}
-          className={isPauseDown && videoList[primaryVideoIndex].secondaryVideo?'show':'hide'}
-          loop
-        >
-          <source ref={secondaryVideoSourceElement} src={videoList[0].secondaryVideo}></source>
-        </video>
+        {
+          videoList.map((slot, key) => (
+            <video
+              id={`slot${key}-secondary`}
+              className={isPauseDown && slotIndex == key ? 'show' : 'hide'}
+              onEnded={() => updateSlotIndex()}
+              key={key}
+              muted
+              autoPlay
+            >
+              <source src={slot.secondaryVideo} />
+            </video>
+          ))
+        }
       </div>
 
       <button
--- services/getVideos.ts
+++ services/getVideos.ts
@@ -4,10 +4,10 @@ import { storage } from "../utils/initFirebase";
 
 const NUMBER_OF_SLOTS: number = 4;
 const MOCK_DATA = [
-    {
-        primaryVideo: 'https://firebasestorage.googleapis.com/v0/b/tempproject-38790.appspot.com/o/derby-pause%2Fslot1%2Fslot-1-primary.webm?alt=media&token=0f8480c4-5bf3-4bf3-b604-2b8e69d8541c',
-        secondaryVideo: ''
-    },
+    // {
+    //     primaryVideo: 'https://firebasestorage.googleapis.com/v0/b/tempproject-38790.appspot.com/o/derby-pause%2Fslot1%2Fslot-1-primary.webm?alt=media&token=0f8480c4-5bf3-4bf3-b604-2b8e69d8541c',
+    //     secondaryVideo: ''
+    // },
     {
         primaryVideo: 'https://firebasestorage.googleapis.com/v0/b/tempproject-38790.appspot.com/o/derby-pause%2Fslot2%2Fslot-2-primary.webm?alt=media&token=e540ebbd-d77a-4688-a1c6-9798590cc482',
         secondaryVideo: 'https://firebasestorage.googleapis.com/v0/b/tempproject-38790.appspot.com/o/derby-pause%2Fslot2%2Fslot-2-secondary.webm?alt=media&token=c6bb4174-8558-4d56-bdf5-471f7676f040'
@@ -34,22 +34,22 @@ const getVideoLink = async (fileRef: string): Promise<string> => {
 }
 
 export const getVideoList = async (): Promise<Slot[]> => {
-    // let videoList: Slot[] = [];
+    let videoList: Slot[] = [];
 
-    // try {
-    //     for (var i = 1; i <= NUMBER_OF_SLOTS; i++) {
-    //         const primaryVideoRef = `derby-pause/slot${i}/slot-${i}-primary.webm`;
-    //         const secondaryVideoRef = `derby-pause/slot${i}/slot-${i}-secondary.webm`;
-    //         videoList.push({
-    //             primaryVideo: await getVideoLink(primaryVideoRef),
-    //             secondaryVideo: await getVideoLink(secondaryVideoRef)
-    //         })
-    //     }
-    // } catch (error) {
-    //     console.log(error);
-    // }
+    try {
+        for (var i = 0; i < NUMBER_OF_SLOTS; i++) {
+            const primaryVideoRef = `derby-pause/slot${i}/slot${i}-primary.webm`;
+            const secondaryVideoRef = `derby-pause/slot${i}/slot${i}-secondary.webm`;
+            videoList.push({
+                primaryVideo: await getVideoLink(primaryVideoRef),
+                secondaryVideo: await getVideoLink(secondaryVideoRef)
+            })
+        }
+    } catch (error) {
+        console.log(error);
+    }
 
-    // return videoList;
+    return videoList;
 
-    return MOCK_DATA;
+    // return MOCK_DATA;
 }
